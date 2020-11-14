4732f3785a0a26f6fc5728f627eeb56b6827fdbb
decrystal <2399274254lsc@gmail.com>
2020-11-15 01:45:45 +0800

    增加接收搜索的商家数组

--- mutation-types.js
+++ mutation-types.js
@@ -7,3 +7,4 @@ export const RESET_USERS = 'reset_users'//重置users
 export const RECIEVE_SHOPSFOODS = 'reset_shopsFoods'//获取点餐信息
 export const RECIEVE_SHOPSRATINGS = 'reset_shopRatings'//获取评价信息
 export const RECIEVE_SHOPSINFO = 'reset_shopInfo'//获取商家信息
+export const RECEIVE_SEARCH_SHOPS = 'receive_search_shops' // 接收搜索的商家数组
